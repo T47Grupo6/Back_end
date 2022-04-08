b43b14eb0ef35dc42d218268434190a8e490b04c
SeijiTom <thiago.tomita2@gmail.com>
2022-04-08 17:06:42 -0300

    Adicionando pasta java na pasta main

---
+++ src/main/java/com/generation/savenature/SavenatureApplication.java
@@ -0,0 +1,13 @@
+package com.generation.savenature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavenatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavenatureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/savenature/model/Categoria.java
@@ -0,0 +1,70 @@
+package com.generation.savenature.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nomeCategoria é Obrigatório e não pode utilizar espaços em branco!") 
+	@Size(min = 3, max = 100, message = "O atributo nomeCategoria deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String nomeCategoria; 
+	
+	@NotNull(message = "O atributo descricao é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo descricao deve conter no mínimo 10 e no máximo 500 caracteres")
+	private String descricaoCategoria;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public String getDescricaoCategoria() {
+		return descricaoCategoria;
+	}
+
+	public void setDescricaoCategoria(String descricaoCategoria) {
+		this.descricaoCategoria = descricaoCategoria;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+}
